f1f233947329dab3f4cea41d25be9b6d88c6a064
Oliver Valter <olva0001@stud.kea.dk>
2026-05-25 12:29:03 +0200

    login design done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import { useLogin } from "../hooks/useAuth"
 
 import { Input } from "@/app/components/ui/Input"
 import { Button } from "@/app/components/ui/Button"
+import { Checkbox } from "../components/ui/Checkbox"
 
 export default function LoginPage() {
   const login = useLogin()
@@ -22,8 +23,8 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-        <div className="bg-[var(--card)] min-h-screen px-6 flex items-start justify-center pt-15 mt-60 rounded-t-[36px]">
+    <div className="h-screen overflow-hidden bg-black pt-50">
+        <div className="bg-[var(--card)] h-full px-6 flex items-start justify-center py-15 rounded-t-[36px]">
             <form className="flex flex-col gap-4 w-full">
                 <h1 className="text-center mb-5">Login</h1>
 
@@ -43,9 +44,33 @@ export default function LoginPage() {
                 onChange={(e) => setPassword(e.target.value)}
                 />
 
+                <div className="flex items-center justify-between">
+                    <Checkbox
+                    name="remember"
+                    label="Husk mig"
+                    />
+
+                    <a
+                    href="/forgot-password"
+                    className="text-sm text-text-secondary underline"
+                    >Glemt password?
+                    </a>
+                </div>
+
                 <Button type="submit">
                 Login
                 </Button>
+
+                <p className="text-center text-sm text-text-secondary mt-4">
+                Har du ikke en profil?{" "}
+                
+                <a
+                    href="/sign-up"
+                    className="font-bold text-text-secondary"
+                >
+                    Opret bruger
+                </a>
+                </p>
             </form>
         </div>
     </div>
