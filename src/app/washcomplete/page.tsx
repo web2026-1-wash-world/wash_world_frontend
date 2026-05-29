e1160164f56c26fd8ced1c7b5e8e80409b4a4fea
Rasmus <rame0001@stud.kea.dk>
2026-05-29 12:37:41 +0200

    Ændring i Card komponent og oprettelse af completewash page

---
+++ src/app/washcomplete/page.tsx
@@ -0,0 +1,46 @@
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+import { FaStar } from "react-icons/fa";
+
+import { Card } from "../components/ui/Card";
+import { XPCard } from "../components/cards/XPCard";
+
+export default function washComplete() {
+    return (
+        <div className="flex flex-col justify-center items-center gap-4">
+            <IoCheckmarkCircleSharp
+            className="text-6xl fill-(--color-green-on-black)"
+            />
+            <h1>Vasken er færdig</h1>
+            <p className="text-(--color-text-secondary)">Herlev  · I dag 14:32 </p>
+            <Card>
+                <div className="w-full justify-center flex items-center">
+                    <p className="text-(--color-text-secondary)">Bedøm din vask</p>
+                </div>
+                <div className="flex justify-between text-4xl px-8 py-2">
+                <FaStar
+                    className="fill-(--color-brand-green)"
+                    />
+                    <FaStar
+                    className="fill-(--color-brand-green)"
+                    />
+                    <FaStar
+                    className="fill-(--color-brand-green)"
+                    />
+                    <FaStar
+                    className="fill-(--color-brand-green)"
+                    />
+                    <FaStar
+                    className="fill-(--color-brand-green)"
+                />
+                </div>
+            </Card>
+            <XPCard
+                title="Clean streak"
+                washCount={7}
+                currentXP={65}
+                nextRewardXP={30}
+            >
+            </XPCard>
+        </div>
+    )
+}
