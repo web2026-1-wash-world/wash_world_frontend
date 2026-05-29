551ecd78c71e30832780c4994edafb1736751f61
Rasmus <rame0001@stud.kea.dk>
2026-05-29 15:33:16 +0200

    Span bar

---
+++ src/app/duringwash/page.tsx
@@ -0,0 +1,22 @@
+export default function duringWash() {
+    return (
+        <div>
+        <div className="w-97.5">
+            <div className="flex gap-0.5 *:bg-(--color-surface) *:h-1.5 *:w-full *:rounded-full">
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+            </div>
+            <div className="w-full flex gap-0.5 *:bg-(--color-green-on-black) *:h-1.5 *:w-full *:rounded-full absolute z-1 top-0 left-0">
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+            </div>
+        </div>
+        </div>
+    )
+}
